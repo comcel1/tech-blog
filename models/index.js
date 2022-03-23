afb125d1dc08f57de21d4b654243dd6b05df4e24
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-03-23 17:00:20 -0500

    added associations for user, comment and post

--- models/index.js
+++ models/index.js
@@ -1,7 +1,33 @@
 // add associations
+const User = require('./User');
+const Comment = require('./Comment');
+const Post = require('./Post');
 
 // user model (table)
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
 
 // post model
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
 
 // comment model
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Comment, Post };

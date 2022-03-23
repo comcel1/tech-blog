46bb1c3d2e5546c41e0c248c53416b0aebf24b94
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-03-23 16:32:35 -0500

    added post model

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,27 @@ const sequelize = require('../config/connection');
 class Post extends Model {}
 
 Post.init(
-  {},
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'user', key: 'id' },
+    },
+  },
   {
     sequelize,
     freezeTableName: true,

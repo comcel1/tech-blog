baff87535b187723f8c71c318064ca9151dc175e
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-03-24 17:34:01 -0500

    created homepage view (handlebars)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,18 @@ const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  Post.findAll({
+    attributes: ['id', 'title', 'post_text', 'createdAt'],
+    include: { model: User, attributes: ['username'] },
+  })
+    .then((data) => {
+      const posts = data.map((post) => post.get({ plain: true }));
+      res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
